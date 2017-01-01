d3ae2fb7011bb764b6f6fda942c51294e884c61e
Darescu Ionut <ionut.darescu@gmail.com>
2017-01-02 01:22:18 +0200

    Compile latest SoftetherVPN and update script

--- SoftEtherVPN.sh
+++ SoftEtherVPN.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-mkdir ~/softether && cd ~/softether
+mkdir -p ~/softether && cd ~/softether
 
 BASE=`pwd`
 SRC=$BASE/src
@@ -16,22 +16,22 @@ CXXFLAGS=$CFLAGS
 CONFIGURE="./configure --prefix=/opt --host=arm-linux"
 MAKE="make -j`nproc`"
 
-mkdir $SRC
+mkdir -p $SRC
 
 ######## ####################################################################
 # ZLIB # ####################################################################
 ######## ####################################################################
 
-mkdir $SRC/zlib && cd $SRC/zlib
-$WGET http://zlib.net/zlib-1.2.8.tar.gz
-tar zxvf zlib-1.2.8.tar.gz
-cd zlib-1.2.8
+mkdir -p $SRC/zlib && cd $SRC/zlib
+$WGET http://zlib.net/zlib-1.2.9.tar.gz
+tar zxvf zlib-1.2.9.tar.gz
+cd zlib-1.2.9
 
 LDFLAGS=$LDFLAGS \
 CPPFLAGS=$CPPFLAGS \
 CFLAGS=$CFLAGS \
 CXXFLAGS=$CXXFLAGS \
-CROSS_PREFIX=arm-linux- \
+CROSS_PREFIX=arm-linux-gnueabi- \
 ./configure \
 --prefix=/opt \
 --static
@@ -44,26 +44,32 @@ make install DESTDIR=$BASE
 ########### #################################################################
 
 mkdir -p $SRC/openssl && cd $SRC/openssl
-$WGET https://www.openssl.org/source/openssl-1.0.2h.tar.gz
-tar zxvf openssl-1.0.2h.tar.gz
-cd openssl-1.0.2h
+$WGET https://www.openssl.org/source/openssl-1.0.2j.tar.gz
+tar zxvf openssl-1.0.2j.tar.gz
+cd openssl-1.0.2j
 
 ./Configure linux-armv4 -march=armv7-a -mtune=cortex-a9 \
 --prefix=/opt zlib \
 --with-zlib-lib=$DEST/lib \
 --with-zlib-include=$DEST/include
 
-make CC=arm-linux-gcc
-make CC=arm-linux-gcc install INSTALLTOP=$DEST OPENSSLDIR=$DEST/ssl
+make CC=arm-linux-gnueabi-gcc
+make CC=arm-linux-gnueabi-gcc install INSTALLTOP=$DEST OPENSSLDIR=$DEST/ssl
 
 ########### #################################################################
 # NCURSES # #################################################################
 ########### #################################################################
 
-mkdir $SRC/curses && cd $SRC/curses
+mkdir -p $SRC/curses && cd $SRC/curses
 $WGET http://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz
 tar zxvf ncurses-6.0.tar.gz
 cd ncurses-6.0
+
+export TARGETMACH=arm-linux-gnueabi
+export CROSS=arm-linux-gnueabi
+export CC=${CROSS}-gcc
+export LD=${CROSS}-ld
+export AS=${CROSS}-as
 
 LDFLAGS=$LDFLAGS \
 CPPFLAGS="-P $CPPFLAGS" \
@@ -84,12 +90,12 @@ make install DESTDIR=$BASE
 # LIBREADLINE # #############################################################
 ############### #############################################################
 
-mkdir $SRC/libreadline && cd $SRC/libreadline
+mkdir -p $SRC/libreadline && cd $SRC/libreadline
 $WGET http://ftp.gnu.org/gnu/readline/readline-6.3.tar.gz
 tar zxvf readline-6.3.tar.gz
 cd readline-6.3
 
-$WGET https://raw.githubusercontent.com/lancethepants/tomatoware/master/patches/readline/readline.patch
+$WGET https://raw.githubusercontent.com/johnnyslt/SoftEtherVPN-arm-static/master/patches/readline.patch
 patch < readline.patch
 
 LDFLAGS=$LDFLAGS \
@@ -108,10 +114,14 @@ make install DESTDIR=$BASE
 # LIBICONV # ################################################################
 ############ ################################################################
 
-mkdir $SRC/libiconv && cd $SRC/libiconv
+mkdir -p $SRC/libiconv && cd $SRC/libiconv
 $WGET http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.14.tar.gz
 tar zxvf libiconv-1.14.tar.gz
 cd libiconv-1.14
+
+$WGET https://raw.githubusercontent.com/johnnyslt/SoftEtherVPN-arm-static/master/patches/libiconv.patch
+
+patch srclib/stdio.in.h < libiconv.patch
 
 LDFLAGS=$LDFLAGS \
 CPPFLAGS=$CPPFLAGS \
@@ -124,18 +134,24 @@ $CONFIGURE \
 $MAKE
 make install DESTDIR=$BASE
 
+unset TARGETMACH
+unset CROSS
+unset CC
+unset LD
+unset AS
+
 ############# ###############################################################
 # SOFTETHER # ###############################################################
 ############# ###############################################################
 
-mkdir $SRC/softether && cd $SRC/softether
+mkdir -p $SRC/softether && cd $SRC/softether
 git clone https://github.com/SoftEtherVPN/SoftEtherVPN.git
 
 cd SoftEtherVPN
-git checkout c0c1b914db8d27fa2f60fb88ee45b032b881aa28
+#git checkout c0c1b914db8d27fa2f60fb88ee45b032b881aa28
 cd ..
 
-cp -r SoftEtherVPN SoftEtherVPN_host
+cp -rfv SoftEtherVPN SoftEtherVPN_host
 cd SoftEtherVPN_host
 
 if [ "`uname -m`" == "x86_64" ];then
@@ -148,15 +164,15 @@ $MAKE
 
 cd ../SoftEtherVPN
 
-$WGET https://raw.githubusercontent.com/el1n/OpenWRT-package-softether/master/softethervpn/patches/100-ccldflags.patch
-$WGET https://raw.githubusercontent.com/lancethepants/SoftEtherVPN-arm-static/master/patches/iconv.patch
+$WGET https://raw.githubusercontent.com/johnnyslt/SoftEtherVPN-arm-static/master/patches/100-ccldflags.patch
+$WGET https://raw.githubusercontent.com/johnnyslt/SoftEtherVPN-arm-static/master/patches/iconv.patch
 patch -p1 < 100-ccldflags.patch
 patch -p1 < iconv.patch
 
 cp ./src/makefiles/linux_32bit.mak ./Makefile
-sed -i 's,#CC=gcc,CC=arm-linux-gcc,g' Makefile
+sed -i 's,#CC=gcc,CC=arm-linux-gnueabi-gcc,g' Makefile
 sed -i 's,-lncurses -lz,-lncursesw -lz -liconv -ldl,g' Makefile
-sed -i 's,ranlib,arm-linux-ranlib,g' Makefile
+sed -i 's,ranlib,arm-linux-gnueabi-ranlib,g' Makefile
 
 CCFLAGS="$CPPFLAGS $CFLAGS" \
 LDFLAGS="-static $LDFLAGS" \
